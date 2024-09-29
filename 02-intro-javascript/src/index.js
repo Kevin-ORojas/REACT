c5009b9609147e95386a644e48e886a2a836c955
NozoDev <kevinroj326@gmail.com>
2024-09-28 21:42:56 -0500

    operador condicional ternario

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,7 @@
+const activo = false;
+
+// const mensaje = !activo ? "esta activo" : "estado no activo";
+
+const mensaje = !activo && "estado activo"; // este operador es una forma corta de hacer un IF o un = ?   :
+
+console.log(mensaje);

7ad9ac69a8e3fec213a08f4fa4bff016d8a7596d
NozoDev <kevinroj326@gmail.com>
2024-10-17 20:37:24 -0500

    usecallback

---
+++ 05-hook-app/src/06-memos/CallbackHook.jsx
@@ -0,0 +1,29 @@
+import React, { useCallback, useState } from "react";
+import { ShowIncrement } from "./ShowIncrement";
+
+/* el use callback sirve para memorizar funciones.*/
+
+export const CallbackHook = () => {
+  const [counter, setCounter] = useState(10);
+
+  //   const incrementFather = () => {
+  //     setCounter(counter + 1);
+  //   };
+
+  /* El useCallback nos devuelve una versión optimizada de la función que se pasa como argumento.
+  Esta versión se utiliza en lugar de la original cuando se pasa como prop a un componente o cuando se utiliza como dependencia de un useEffect. */
+
+  const incrementFather = useCallback(() => {
+    setCounter((value) => value + 1);
+  }, []);
+
+  return (
+    <div>
+      <h2>CallbackHook: {counter}</h2>
+
+      <hr />
+
+      <ShowIncrement increment={incrementFather} />
+    </div>
+  );
+};
--- 05-hook-app/src/HookApp.jsx
+++ 05-hook-app/src/HookApp.jsx
@@ -8,6 +8,7 @@ import { FocusScreen } from "./04-useRef/FocusScreen";
 import { Layout } from "./05-useLayoutEffect/Layout";
 import { Memorize } from "./06-memos/Memorize";
 import { MemoHook } from "./06-memos/MemoHook";
+import { CallbackHook } from "./06-memos/CallbackHook";
 
 export const HookApp = () => {
   return (
@@ -31,7 +32,9 @@ export const HookApp = () => {
 
       {/* <Memorize /> */}
 
-      <MemoHook />
+      {/* <MemoHook /> */}
+
+      <CallbackHook />
     </div>
   );
 };

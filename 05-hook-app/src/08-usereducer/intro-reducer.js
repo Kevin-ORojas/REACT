d467ecd2f611e3686bcb28f3a6fb04d9b6940dce
NozoDev <kevinroj326@gmail.com>
2024-10-25 22:48:37 -0500

    intro reducer.

--- 05-hook-app/src/08-usereducer/intro-reducer.js
+++ 05-hook-app/src/08-usereducer/intro-reducer.js
@@ -1,1 +1,34 @@
 console.log("USE REDUCER");
+
+const initialValue = [
+  {
+    id: 1,
+    todo: "Piedra del alma",
+    done: false,
+  },
+];
+
+const todoReducer = (state = initialValue, action = {}) => {
+  if (action.type === "[TODO] add todo") {
+    return [...state, action.payload];
+  }
+
+  return state;
+};
+
+let todos = todoReducer();
+
+const newTodo = {
+  id: 2,
+  todo: "recolectar piedra del poder",
+  done: false,
+};
+
+const addTodoAction = {
+  type: "[TODO] add todo", // esta va hacer la accion
+  payload: newTodo, // y esto es lo que va adentro de la accion y si es para borrar todos no hace falta esta linea.
+};
+
+todos = todoReducer(todos, addTodoAction);
+
+console.log({ state: todos });

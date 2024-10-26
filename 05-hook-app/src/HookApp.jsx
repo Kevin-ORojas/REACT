7b765c5b59ffe26810b5adc669c4f283cc4fe565
NozoDev <kevinroj326@gmail.com>
2024-10-25 22:32:38 -0500

    use reducer

--- 05-hook-app/src/HookApp.jsx
+++ 05-hook-app/src/HookApp.jsx
@@ -9,7 +9,7 @@ import { Layout } from "./05-useLayoutEffect/Layout";
 import { Memorize } from "./06-memos/Memorize";
 import { MemoHook } from "./06-memos/MemoHook";
 import { CallbackHook } from "./06-memos/CallbackHook";
-import { Padre } from "../07-tarea-memo/Padre";
+import { Padre } from "./07-tarea-memo/Padre";
 
 export const HookApp = () => {
   return (
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { HookApp } from "./HookApp.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HookApp />
-  </StrictMode>
-);
+import "./08-usereducer/intro-reducer.js";
+createRoot(document.getElementById("root"))
+  .render
+  // // <StrictMode>
+  // <HookApp />
+  // // </StrictMode>
+  ();
